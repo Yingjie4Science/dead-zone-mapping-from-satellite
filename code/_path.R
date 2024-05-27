61d10d9899f3521b8be48c650f466dd8c7e9fa6a
Yingjie Li <yingjieli.edu@gmail.com>
2024-05-26 17:06:49 -0700

    minor edits

--- code/_path.R
+++ code/_path.R
@@ -1,5 +1,25 @@
 
+library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(lubridate)
 
+path <- rstudioapi::getSourceEditorContext()$path
+dir  <- dirname(rstudioapi::getSourceEditorContext()$path)
+dir.root <- dirname(dir)
 
 dir.foodfootprint <- "D:/_papers/_phd_dissertation/food_footprints/"
 dir.fert.data <- paste0(dir.foodfootprint, "data/ag_fertilizer/USGS_N_P_FertilizerManure_county1950_2017/")
+
+dir.fig     <- paste0(dir.root, '/figures/')
+
+
+## keep more decimals
+options(digits = 15)
+options(pillar.sigfig = 15)
+
+### Disable Scientific Notation in R
+options(scipen = 999) # Modify global options in R
+
+today <- format(Sys.time(), "%Y%m%d"); print(today)
+
